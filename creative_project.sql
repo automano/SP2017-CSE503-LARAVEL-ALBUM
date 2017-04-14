4f14a51fe217495777df4fccd0648aeef7eb036a
Zhening Li <zhening.li@wustl.edu>
2017-04-14 16:34:41 -0500

    laravel can manipulate mysql

--- creative_project.sql
+++ creative_project.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE CREATIVE_PROJECT;
 
 /* create new user to manipulate this database */
-CREATE user 'cse330'@'127.0.0.1' identified BY 'wustl123';
+CREATE user 'cse330'@'localhost' identified BY 'wustl123';
 
 /* grant privileges to a certain user so that they can only manipulate data in a certain database */
-GRANT SELECT,INSERT,UPDATE,DELETE on creativeproject.* to cse330@'127.0.0.1';
+GRANT SELECT,INSERT,UPDATE,DELETE on creativeproject.* to cse330@'localhost';
