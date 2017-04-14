cc9168d83a29aecbebde5c6a0b9baf8324fb9355
Zhening Li <zhening.li@wustl.edu>
2017-04-14 18:03:41 -0500

    1. make:auth 2. create user table and reset password table 3. enable email service 4. user can log in, register and reset user account

--- creative_project.sql
+++ creative_project.sql
@@ -1,8 +1,2 @@
 /* create database */
 CREATE DATABASE CREATIVE_PROJECT;
-
-/* create new user to manipulate this database */
-CREATE user 'cse330'@'localhost' identified BY 'wustl123';
-
-/* grant privileges to a certain user so that they can only manipulate data in a certain database */
-GRANT SELECT,INSERT,UPDATE,DELETE on creativeproject.* to cse330@'localhost';
